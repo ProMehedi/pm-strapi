58d0d4bed7885cca2b9d61340e8c7e4adfe55eb7
promehedi <mmhasan563@gmail.com>
2021-03-18 23:27:41 +0600

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e658e76636812e02c013bb52ca6c0887'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b033e920-a4b5-43e7-9fa8-9f526896daa8'
+};
